cf5b971495b5c97c634c79fe0eccf378172ebaf0
felipe.sampedro <pipesancho182@gmail.com>
2022-09-15 01:23:13 -0500

    completando el desafio con los requerimientos pedidos.... ya falta poco

--- index.js
+++ index.js
@@ -1,66 +1,21 @@
 const express = require('express')
+const apiRoutes = require('./router/app.routers')
 
+const app = express()
 const PORT = process.env.PORT || 8080
 // const products=require('./data.json')
-const fs =require('fs')
+// const fs =require('fs')
 
 const products=JSON.parse(fs.readFileSync('./data.json','utf-8'))
 
-const app = express()
 
 //Middlewares
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 
 //Rutas
-app.get('/',(req,res,next)=>{
-    res.send({name:"Felipe"})
-})
-
-
-app.get('/products',(req,res,next)=>{
-    const {price}=req.query
-    console.log(req.query);
-    const priceNumber=parseInt(price || 99999999)
-    const restProducts = products.filter((p)=>{
-        return p.price < priceNumber})
-    res.json(restProducts)
-})
-
-app.get('/products/:id',(req,res,next)=>{
-    console.log(req.params);
-    const product = products.find((p)=>p.id=== +(req.params.id))
-    res.json(product)
-
-})
-
-
-app.post('/products',(req,res,next)=>{
-    console.log(req.body);
-    const {title,price,thumbnail} = req.body;
-    const newProduct={
-        title,
-        price,
-        thumbnail,
-        id:products.length +1,
-    }
-
-    products.push(newProduct)
-    fs.writeFileSync('./data.json',JSON.stringify(products,null,2))
-    res.json(newProduct)
-})
-
-
-app.delete('/products/:id',(req,res,next)=>{
-    const {id} = req.params;
-    const indProduct = products.findIndex((p)=>p.id=== +(id))
-    if (indProduct<0){
-        return res.status(400).send(`Producto con id ${id} no existe`)
-    }
-    const deletedProduct = products.splice(indProduct,1)
-    fs.writeFileSync('./data.json',JSON.stringify(products,null,2))
-    res.json(deletedProduct)
-})
+app.use('/api',apiRoutes)
 
 
 const connectedServer = app.listen(PORT,()=>{
---
+++ router/products/products.routes.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const fs =require('fs')
+
+const { products } = require('../../data/data');
+
+const router = express.Router()
+
+
+
+router.get('/',(req,res,next)=>{
+    console.log(req.query);
+    res.json(products)
+})
+
+router.get('/:id',(req,res,next)=>{
+    console.log(req.params);
+    const idProduct = products.findIndex((p)=>p.id=== +(id))
+    if (idProduct<0){
+        return res.status(400).send(`Producto con id ${id} no existe`)
+    }
+    const product = products.find((p)=>p.id=== +(req.params.id))
+    res.json(product)
+
+})
+
+
+router.post('/',(req,res,next)=>{
+    console.log(req.body);
+    const {title,price,thumbnail} = req.body;
+    const newProduct={
+        title,
+        price,
+        thumbnail,
+        id:products.length +1,
+    }
+
+    products.push(newProduct)
+    fs.writeFileSync('./data.json',JSON.stringify(products,null,2))
+    res.json(newProduct)
+})
+
+
+
+router.put('/:id',(req,res,next)=>{
+    const {id, title, price, thumbnail} = req.params;
+    const indProduct = products.findIndex((p)=>p.id=== +(id))
+    if (indProduct<0){
+        return res.status(400).send(`Producto con id ${id} no existe`)
+    }
+    const actProduct = {  
+        ...products[indProduct],
+        title,
+        price,
+        thumbnail
+    };
+    products[indProduct]=actProduct;
+    fs.writeFileSync('./data.json',JSON.stringify(products,null,2))
+    res.json(deletedProduct)
+})
+
+
+router.delete('/:id',(req,res,next)=>{
+    const {id} = req.params;
+    const indProduct = products.findIndex((p)=>p.id=== +(id))
+    if (indProduct<0){
+        return res.status(400).send(`Producto con id ${id} no existe`)
+    }
+    const deletedProduct = products.splice(indProduct,1)
+    fs.writeFileSync('./data.json',JSON.stringify(products,null,2))
+    res.json(deletedProduct)
+})
+
+
+
+
+
+module.exports = router
